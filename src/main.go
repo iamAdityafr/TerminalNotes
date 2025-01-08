f0c372c197d98d327a880f5ba210049d35bffade
Adityarajsinh Zala <adityarajza@gmail.com>
2025-01-08 10:26:58 +0530

    Update main.go

--- src/main.go
+++ src/main.go
@@ -1,1 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func main() {
+	store := new(Store)
+	if err := store.Init(); err != nil {
+		log.Fatalf("unable to initialise store: %v", err)
+	}
+
+	p := tea.NewProgram(initialModel(store))
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("unable to run tui: %v", err)
+		os.Exit(1)
+	}
+}
 
